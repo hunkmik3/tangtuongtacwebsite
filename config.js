1e70330473f919abdc5daab99a8e87c72d890cbb
Hunk Mike <135243373+hunkmik3@users.noreply.github.com>
2025-08-11 01:40:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // Global frontend config for API endpoint.
 // Ví dụ production:
 // window.API_BASE_URL = 'https://api.your-backend.com';
-
-window.API_BASE_URL = window.API_BASE_URL || '';
+window.API_BASE_URL = 'https://tangtuongtacwebsite.onrender.com'
